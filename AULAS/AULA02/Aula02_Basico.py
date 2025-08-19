7f5c0d4654cfc8936c35e8ffceaa3581445715fb
Wanderson Ribeiro <mecanica.wanderson@gmail.com>
2025-08-19 09:48:17 -0300

    Aula02 - Python

---
+++ AULAS/AULA02/Aula02_Basico.py
@@ -0,0 +1,45 @@
+a:int = 20
+b = 20.5
+
+print ("ABS(a):", abs(a))
+print ("hex(a):", hex(a))
+print ("oct(a):", oct(a))
+print ("bin(a):", bin(a))
+print("str(a):", str(a))
+print("float(a):", float(a))
+
+
+#exercicios
+print("*"*30)
+print("Exercicios")
+print("*"*30)
+
+
+c = 20+30
+print (c)
+
+d = "o valor do numero e: " + str(30)
+print (d)
+
+'''e = int("DB75C", 16)
+print(e)'''
+f = "DB75C"
+g = int(f, 16)
+print(g)
+
+e = hex(898908)
+print(e)
+
+tam = len("Ola mundo")
+print(tam)
+
+conv = int("153") + 160
+print(conv)
+
+encontrar = "PROVA NLP"
+print(encontrar.find("P",2))
+
+print("-"*50)
+
+
+
